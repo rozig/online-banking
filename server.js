7ddc3bc6875d202f4f98ee1edd89cd5313975d66
Ganzorig Erdenebat <the.ganzorig25@gmail.com>
2018-04-03 01:13:47 -0500

    bug fix

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //Install express server
 const express = require('express');
+const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
@@ -7,3 +8,7 @@ app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+app.get('*/all', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
