725c98e4678ac1835c2340caec2ac1f0b1b05b69
Ganzorig Erdenebat <the.ganzorig25@gmail.com>
2018-04-03 01:35:04 -0500

    path fix

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
-app.get('*/all', function(req, res) {
+app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
